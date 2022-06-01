10a8f3b425a63b831c3b6753201e2d491a20764a
paper <faition2@gmail.com>
2022-06-01 15:08:54 +0800

    feat: initial map feature

--- src/ts/map.ts
+++ src/ts/map.ts
@@ -15,8 +15,8 @@ socket.addEventListener('message', (event: MessageEvent<string>) => {
 });
 
 const map: Map = leaflet.map('map').setView([23.5, 121], 7);
-
-console.log(dataJson);
+let longitude: number;
+let latitude: number;
 
 // 設定地圖圖磚
 leaflet.tileLayer(
@@ -25,6 +25,7 @@ leaflet.tileLayer(
 ).addTo(map);
 
 const markers: MarkerClusterGroup = leaflet.markerClusterGroup();
+const allMarkersDataMap: { [key: string]: {} } = {};
 
 dataJson.forEach((item) => {
   const lng:string = item['經度'];
@@ -32,23 +33,44 @@ dataJson.forEach((item) => {
   const marker: Marker = leaflet.marker([Number(lat), Number(lng)], {
     title: item['醫事機構名稱'],
   });
-  marker.bindPopup(`<h1>${item['醫事機構名稱']}</h1>`);
+  marker.bindPopup(`<h3>${item['醫事機構名稱']}</h3><p>快篩剩餘數量：${item['快篩試劑截至目前結餘存貨數量']}</p>`);
+  marker.on('click', (e) => {
+    const dialog = document.getElementById('dialog');
+    const isDialogOpen: boolean = dialog!.classList.contains('is-active');
+    if (!isDialogOpen) {
+      dialog!.classList.add('is-active');
+    }
+    // !. 是告訴 typescript，document.getElementById('title') 不會是 null 或 undefined
+    document.getElementById('title')!.textContent = item['醫事機構名稱'];
+    document.getElementById('brand')!.textContent = item['廠牌項目'];
+    document.getElementById('num')!.textContent = item['快篩試劑截至目前結餘存貨數量'];
+    document.getElementById('address')!.textContent = item['醫事機構地址'];
+    document.getElementById('tel')!.textContent = item['醫事機構電話'];
+    document.getElementById('tips')!.textContent = item['備註'];
+    document.getElementById('time')!.textContent = item['來源資料時間'];
+    const aLink = document.getElementById('googlemap') as HTMLAnchorElement;
+    // https://developers.google.com/maps/documentation/urls/get-started
+    aLink.href = `https://www.google.com/maps/dir/?api=1&origin${latitude},${longitude}&destination=${lat},${lng}&travelmode=driving`
+  });
+  allMarkersDataMap[item['醫事機構名稱']] = item;
   markers.addLayer(marker);
 });
 
 map.addLayer(markers);
 
 navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
-  console.log(position);
-  const { longitude, latitude }: { longitude: number, latitude: number } = position.coords;
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude;
   const circle: Circle = leaflet.circle([latitude, longitude], {
     radius: 20,
     color: 'red',
     fillOpacity: 0.5,
   }).addTo(map);
   map.setView([latitude, longitude], 15);
+  document.querySelector('.loading-container')!.classList.add('is-inactive');
 }, () => {
   console.log('whooppppps error');
+  document.querySelector('.loading-container')!.classList.add('is-inactive');
 });
 
 // leaf cluster
